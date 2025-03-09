5609399ad35ed0d58f49a0546d1ae94397f8ba6e
CJ Crisostomo <25crisostomocj@gmail.com>
2025-03-09 20:15:15 +0800

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,19 @@ const Navbar = () => {
   return (
     <div>
     <div className='cotainer-fluid bg-dark'>
-        <nav className='container navbar navbar-expand navbar-dark text-light'>
+        <nav className='container navbar navbar-expand navbar-dark text-light justify-content-between'>
             <div>
-                <h2 className=''>Logo</h2>
+                <h2 className=''>WeSpeak</h2>
             </div>
 
-                <div className="navbar-nav nav-underline ms-auto">
-                    <a href="" className="nav-link">Home</a>
-                    <a href="" className="nav-link">Home</a>
-                    <a href="" className="nav-link">Home</a>
-                    <a href="" className="nav-link">Home</a>
-                </div>
+            <div className="navbar-nav nav-underline">
+                <a href="" className="nav-link">Home</a>
+                <a href="" className="nav-link">Dashoard</a>
+            </div>
+
+            <div>
+                <button className='btn btn-primary'>Connect To MetaMask</button>
+            </div>
         </nav>
 
     </div>
